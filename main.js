afbbb70e837972af9fcdefb45725498b69b4beeb
null <null>
2024-07-25 10:57:51 +0900

    varでの変数宣言まで

---
+++ main.js
@@ -0,0 +1,13 @@
+/* const, letの宣言　*/
+var val1 = "変数の名前";
+
+console.log(val1);
+
+val1 = "上書きされたvar";
+
+console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "再々宣言した変数に上書きされる";
+console.log(val1);
+console.log(val1);
